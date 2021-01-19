3fc6c8eab546cc19696a293ab2828b2e30200825
Deyvid-g-lins <deyvid.g.lins@accenture.com>
2021-01-19 11:40:31 -0300

    Files

--- DockerfileDeyvid.cp
+++
@@ -1,5 +0,0 @@
-FROM deyvidglins/petclinic
-RUN docker container run --name petclinic_01 -d --restart=always -p 8080:8080 -p 50000:50000 -u 0 deyvidglins/petclinic
-RUN docker run -e MYSQL_USER=petclinic -e MYSQL_PASSWORD=petclinic -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=petclinic -p 3306:3306 mysql:5.7.8
-RUN dokcer exec -it petclinic_01 bash
-RUN docker login && /  docker push docker push deyvidglins/petclinic
--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,7 +1,7 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
+INSERT IGNORE INTO vets VALUES (1, 'Deyvid', 'Lins');
 INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
-INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
+INSERT IGNORE INTO vets VALUES (4, 'Lucas', 'Ortega');
 INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
 INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
